ab8c088d78a6ecd19004fc299947e9dde275c4b5
jyoo0515 <jasonyoo0515@gmail.com>
2021-09-20 00:20:00 +0900

    added comment report feature

---
+++ VectorWebsite.Persistance/Migrations/20210919141257_CommentReport.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VectorWebsite.Persistance.Migrations
+{
+    public partial class CommentReport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Report",
+                table: "PetitionComments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Report",
+                table: "PetitionComments");
+        }
+    }
+}
