3e39b27b696a4773aade2a38445444efd5e4e2e9
jyoo0515 <jasonyoo0515@gmail.com>
2021-10-05 18:15:02 +0900

    User info update

--- VectorWebsite.Domain/ApplicationUser.cs
+++ VectorWebsite.Domain/ApplicationUser.cs
@@ -10,5 +10,24 @@ namespace VectorWebsite.Domain
         public bool ConfirmedStudent { get; set; }
         public string Token { get; set; }
         public string StudentId { get; set; }
+        public Department Department { get; set; }
+    }
+
+    public enum Department
+    {
+        신소재공학과 = 1,
+        화공생명공학과 = 2,
+        기계4반 = 4,
+        컴퓨터과학과 = 5,
+        전기전자6반 = 6,
+        기계7반 = 7,
+        산업공학과 = 8,
+        전기전자9반 = 9,
+        전기전자10반 = 10,
+        사회환경시스템공학과 = 11,
+        건축공학과 = 12,
+        도시공학과 = 13,
+        글로벌융합공학부 = 14,
+        시스템반도체공학과 = 15,
     }
 }
